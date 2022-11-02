f0f4c2bfff6f6328f09632a61cf978399077f02d
Luiz Willner <luiz.claudio@injunior.com.br>
2022-11-01 21:47:49 -0300

    Começo do escalonador FIFO - otimizações feitas

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ arq_entrada = open("entrada/processos.in", 'r')
 processos = processarEntrada(arq_entrada)
 print(processos)
 
-processos = sorted(processos, key=attrgetter("tempo_admissao")) # organiza por tempo de entrada 
-escalonador(processos, 0)
+processos = sorted(processos, key=attrgetter("tempo_admissao"))  # organiza por tempo de entrada
+escalonadorFIFO(processos, 0)
 
 arq_entrada.close()
--- scripts/escalonador_fifo.py
+++ scripts/escalonador_fifo.py
@@ -1,17 +1,15 @@
-#from scripts.leitor_entrada import *
+# from scripts.leitor_entrada import *
 from operator import itemgetter, attrgetter
 
-global lista_prontos # pode executar
-global lista_espera # esperando I/O
-global executando # executando na CPU
-global tempo
-
+# Variáveis globais
 tempo = 0
 lista_prontos = []
 lista_espera = []
+lista_finalizados = []
 executando = []
 
-def remover(lista_processos, processo):
+
+def removerProcesso(lista_processos, processo):
 
     print(processo.nome_programa)
     print("\n")
@@ -20,7 +18,8 @@ def remover(lista_processos, processo):
         print(lista_processos[i].nome_programa)
 
         if lista_processos[i].nome_programa == processo.nome_programa:
-            lista_processos.remove(processo)
+            lista_finalizados.append(lista_processos[i])  # adicionando processo finalizado a lista de finalizados
+            lista_processos.pop(i)  # removendo processo da lista de processos
             print("removido")
             return lista_processos
 
@@ -28,31 +27,29 @@ def remover(lista_processos, processo):
     return lista_processos
 
 
-def escalonador(lista_processos, tempo):
+def escalonadorFIFO(lista_processos, tempo):
     
     while len(lista_processos) != 0:
         # atualiza a fila de prontos pelo tempo de admissão
-        for i in range(len(lista_processos)):
-            if tempo == lista_processos[i].tempo_admissao:
-                lista_prontos.append(lista_processos[i])
+        for processo in lista_processos:
+            if tempo == processo.tempo_admissao:
+                lista_prontos.append(processo)
 
-
-        # confere se existe algum processo em execução #
-
+        # ### confere se existe algum processo em execução ####
         # caso não exista
         if len(executando) == 0:
-            executando.append(lista_prontos[0]) # move o primeiro processo na fila de prontos para e execução
+            executando.append(lista_prontos[0])  # move o primeiro processo na fila de prontos para e execução
             lista_prontos.pop(0)
 
         # caso exista
         else:
             
-            # verifica se acabou a lista de tempos
+            # verifica se acabou a lista de tempos de burst e I/O
             if len(executando[0].burst_io) == 0:
 
-                lista_processos = remover(lista_processos, executando[0]) # remove o processo da lista de processos
+                lista_processos = removerProcesso(lista_processos, executando[0])  # remove o processo da lista de processos
                 executando.clear() 
-                executando.append(lista_prontos[0]) # atualiza a execução com o próximo na lista de prontos
+                executando.append(lista_prontos[0])  # atualiza a execução com o próximo na lista de prontos
                 lista_prontos.pop(0)
 
             # ainda há tempos na lista burst_io
@@ -60,16 +57,14 @@ def escalonador(lista_processos, tempo):
                 
                 # caso o tempo de burst seja maior que 0
                 if executando[0].burst_io[0] > 0:
-                    executando[0].burst_io[0] -= 1 # decrementar em 1 o valor atual
+                    executando[0].burst_io[0] -= 1  # decrementar em 1 o valor atual
 
                 # caso o tempo de burst seja igual a 0
                 if executando[0].burst_io[0] == 0:    
-                    executando[0].burst_io.remove(0) # remove o tempo de burst da lista burst_io
-                    lista_processos = remover(lista_processos, executando[0]) 
+                    executando[0].burst_io.pop(0)  # remove o tempo de burst da lista de tempos de burst I/O
+                    lista_processos = removerProcesso(lista_processos, executando[0])  # TODO: PROVISÓRIO
                     executando.clear()
                     executando.append(lista_prontos[0])
                     lista_prontos.pop(0)
-                    #lista_espera.append(executando)          
+                    # lista_espera.append(executando)
         tempo += 1
-
-
